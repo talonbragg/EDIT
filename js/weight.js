0b37a1a6829e0dfb0598915f56ee2ac5f0644671
Shreyas Lad <shadowtemplates@gmail.com>
2017-02-02 12:22:24 -0800

    Update weight.js

--- js/weight.js
+++ js/weight.js
@@ -1,1 +1,24 @@
+//Copy and Paste
+$(document).ready(function() {
+    var ctrlDown = false,
+        ctrlKey = 17,
+        cmdKey = 91,
+        vKey = 86,
+        cKey = 67;
 
+    $(document).keydown(function(e) {
+        if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+    }).keyup(function(e) {
+        if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+    });
+
+    $(".no-copy-paste").keydown(function(e) {
+        if (ctrlDown && (e.keyCode == vKey || e.keyCode == cKey)) return false;
+    });
+});
+
+$(document).ready(function(bold) {
+  $.ctrl('B', function() {
+    $('body').document.getElementById("body").style.fontWeight = "900";
+  });
+});
